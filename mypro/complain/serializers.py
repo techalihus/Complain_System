42573de1bcc0eddf6932ba9e69debc32c1557152
ali husnain <techali597@gmail.com>
2026-03-16 01:49:37 +0500

    Update serializers code

--- mypro/complain/serializers.py
+++ mypro/complain/serializers.py
@@ -8,13 +8,25 @@ class DepartmentSerializer(serializers.ModelSerializer):
     class Meta:
         model=Department
         fields="__all__"
-
+# ========================
+# Designation Serializer
+# ========================
+class DesignationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Designation
+        fields="__all__"
 # =========================
 # Employee Serializer
 # =========================
 class EmployeeSerializer(serializers.ModelSerializer):
     department=DepartmentSerializer(read_only=True)
     department_id=serializers.PrimaryKeyRelatedField(queryset=Department.objects.all(), source='department', write_only=True)
+    designation = DesignationSerializer(read_only=True)
+    designation_id = serializers.PrimaryKeyRelatedField(
+    queryset=Designation.objects.all(),
+    source="designation",
+    write_only=True
+)
     class Meta:
         model=Employee
         fields="__all__"
@@ -38,6 +50,7 @@ class TicketSerializer(serializers.ModelSerializer):
     employee=EmployeeSerializer(read_only=True)
     employee_id=serializers.PrimaryKeyRelatedField(queryset=Employee.objects.all(), source='employee',
     write_only=True)
+    reply_count = serializers.IntegerField(read_only=True)
     class Meta:
         model=Ticket
         fields="__all__"
@@ -60,6 +73,12 @@ class TicketReplySerializer(serializers.ModelSerializer):
 class TicketHistorySerializer(serializers.ModelSerializer):
     ticket=TicketSerializer(read_only=True)
     ticket_id=serializers.PrimaryKeyRelatedField(queryset=Ticket.objects.all(), source='ticket', write_only=True)
+    changed_by = EmployeeSerializer(read_only=True)
+    changed_by_id = serializers.PrimaryKeyRelatedField(
+    queryset=Employee.objects.all(),
+    source="changed_by",
+    write_only=True
+)
     class Meta:
         model=TicketHistory
         fields="__all__"
@@ -73,6 +92,11 @@ class TicketRatingSerializer(serializers.ModelSerializer):
     class Meta:
         model=TicketRating
         fields="__all__"
+
+    def validate_rating(self, value):
+        if value < 1 or value > 5:
+            raise serializers.ValidationError("Rating must be between 1 and 5")
+        return value
 
 # ========================
 # Designation Serializer
